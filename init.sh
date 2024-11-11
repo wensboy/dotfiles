29e70091776f79df1c287d506d47a4e6127bff5a
wendisx <wendisx158@163.com>
2024-11-11 16:56:07 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,23 +1,92 @@
 #!/usr/bin/env bash
 
-# deine package lists
-PACKAGES=()
+# deine software_packages lists
+SOFTWARE_PACKAGES=(
+    "neovim"
+    "neofetch"
+    "zsh"
+    "i3wm"
+    "rofi"
+    "alacritty"
+    "stow"
+    "picom"
+    "polybar"
+    "brightnessctl"
+    "bluez"
+    "bluez-utils"
+    "feh"
+    "maim"
+    "ranger"
+    "dunst"
+    #what you want to add
+)
 
-# update pacman 
+#define config path
+CONFIG_PATH=(
+    "ranger"
+    "nvim"
+    "i3"
+    "polybar"
+    "rofi"
+    "neofetch"
+    "picom"
+    "alacritty"
+    "dunst"
+    #~/.config/*
+)
+
+#define stow packages
+STOW_PACKAGES=(
+    "ranger"
+    "nvim"
+    "i3"
+    "polybar"
+    "rofi"
+    "neofetch"
+    "picom"
+    "alacritty"
+    "dunst"
+    #~/.dotfiles/*
+)
+
+#change this to match your os
+#like:
+#pacman/yay -S 
+#apt-get install -y
+#dnf install -y
+#...
+COMMAND='pacman -S --noconfirm'
+
+#delete a profile that already exists
+for path in "${CONFIG_PATH[@]}";
+do
+    if [ -d "$HOME/.config/$path" ]
+    then
+	echo "Removing: ~/.config/$path"
+	rm -rf "$HOME/.config/$path"
+    else
+	echo "Removing: ~/.config/$path not found!"
+    fi
+done
+
+
+# update os 
+# sudo apt-get upgrade
+# sudo dnf upgrade
 echo "Updating packages lists..."
 sudo pacman -Syu --noconfirm
 
-# install 
+# install software
 echo "Installing packages..."
 
-for package in "${PACKAGES[@]}"
+for package in "${SOFTWARE_PACKAGES[@]}"
 do
     if ! pacman -Qi "$package" &>/dev/null;
     then
 	echo "Installing $package..."
-	sudo pacman -S --noconfirm "$package"
+	sudo $COMMAND "$package"
     else
-	echo "$package is already installed"
+	echo "$package is already installed!"
     fi
 done
 
@@ -26,4 +95,36 @@ done
 echo "CLeaning up dependecies..."
 sudo pacman -Sc --noconfirm
 
+# install software complete
 echo "successful! installing complete!"
+
+# create config dir
+CREATE_CONFIG_DIR(){
+    for config_dir in "$CONFIG_PATH[@]"
+    do
+	echo "Createing: $config_dir configuration dir..."
+	mkdir $HOME/.config/$config_dir
+    done
+}
+
+# create config link
+CREATE_CONFIG_LINK(){
+    echo "Starting creating configuration link..."
+    cd $HOME/.dotfiles
+    for config_link in "$STOW_PACKAGES[@]"
+    do
+	if [ -d "$HOME/.dotfiles/$config_link"]
+	then
+	    echo "Creating: $config_link configuration link..."
+	    stow $config_link -t "$HOME/.config/$config_link"
+	else
+	    echo "Creating: $config_link not found!"
+	fi
+    done
+}
+
+# Execute functions
+main(){
+    CREATE_CONFIG_DIR()
+    CREATE_CONFIG_LINK()
+}
