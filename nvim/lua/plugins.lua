5915900c5f6a342a8bde64429e8921c008d95a92
wendisx <wendisx158@163.com>
2024-11-19 22:41:04 +0800

    update {nvim sway waybar}

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,32 +1,35 @@
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {"nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+	build = ":TSUpdate"
     },
     {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-    },
-    {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-     {'romgrk/barbar.nvim',
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
 	dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    }
+	    "nvim-lua/plenary.nvim",
+	    "nvim-tree/nvim-web-devicons",
+	    "MunifTanjim/nui.nvim",
+	}
+    },
+    {
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {'romgrk/barbar.nvim',
+	dependencies = {
+	    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+	    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+	}
     },
     {
 	"folke/which-key.nvim"
+    },
+    {
+	"NvChad/nvim-colorizer.lua"
     },
 }
