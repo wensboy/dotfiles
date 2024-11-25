9e0627b258c449f678679418678561dd7ad03c86
wendisx <wendisx158@163.com>
2024-11-25 22:04:05 +0800

    update {init.sh}

--- init.sh
+++ init.sh
@@ -1,136 +1,36 @@
 #!/usr/bin/env bash
 
-# deine software_packages lists
-SOFTWARE_PACKAGES=(
-    "neovim"
-    "neofetch"
-    "zsh"
-    "i3wm"
-    "rofi"
-    "alacritty"
-    "stow"
-    "picom"
-    "polybar"
-    "brightnessctl"
-    "bluez"
-    "bluez-utils"
-    "feh"
-    "maim"
-    "ranger"
-    "dunst"
-    #what you want to add
-)
+#===> packages from arch pacman
+pacman_packages=()
 
-#define config path
-CONFIG_PATH=(
-    "ranger"
-    "nvim"
-    "i3"
-    "polybar"
-    "rofi"
-    "neofetch"
-    "picom"
-    "alacritty"
-    "dunst"
-    #~/.config/*
-)
+#===> packages from AUR(yay,paru...)
+aur_packages=()
+AUR=yay
 
-#define stow packages
-STOW_PACKAGES=(
-    "ranger"
-    "nvim"
-    "i3"
-    "polybar"
-    "rofi"
-    "neofetch"
-    "picom"
-    "alacritty"
-    "dunst"
-    #~/.dotfiles/*
-)
+#===> make sure at $HOME dir currently
+cd $HOME
 
-#change this to match your os
-#like:
-#pacman/yay -S 
-#apt-get install -y
-#dnf install -y
-#...
-COMMAND='pacman -S --noconfirm'
-
-#delete a profile that already exists
-echo "Deletes an existing configuration directory..."
-for path in "${CONFIG_PATH[@]}";
-do
-    if [ -d "$HOME/.config/$path" ]
-    then
-	echo "Removing: ~/.config/$path"
-	rm -rf "$HOME/.config/$path"
-    else
-	echo "Removing: ~/.config/$path not found!"
-    fi
-done
-
-
-# update os 
-# sudo apt-get upgrade
-# sudo dnf upgrade
-echo "Updating packages lists..."
-sudo pacman -Syu --noconfirm
-
-# install software
-echo "Installing packages..."
-
-for package in "${SOFTWARE_PACKAGES[@]}"
-do
-    if ! pacman -Qi "$package" &>/dev/null;
-    then
-	echo "Installing: $package..."
-	eval "sudo $COMMAND $package"
-    else
-	echo "$package already exists!"
-    fi
-done
-
-
-# clean up no useful dependencies 
-echo "CLeaning up dependecies..."
-sudo pacman -Sc --noconfirm
-
-# install software complete
-echo "--->Installing completed!!!<---"
-
-# create config dir
-CREATE_CONFIG_DIR(){
-    echo "Create config dir..."
-    for config_dir in "$CONFIG_PATH[@]"
-    do
-	echo "Createing: $config_dir"
-	mkdir $HOME/.config/$config_dir
-    done
+#===> start install function
+Start_install(){
+    echo "[Start_install]===> $1"
 }
 
-# create config link
-CREATE_CONFIG_LINK(){
-    echo "Create config link..."
-    cd $HOME/.dotfiles
-    for config_link in "$STOW_PACKAGES[@]"
-    do
-	if [ -d "$HOME/.dotfiles/$config_link"]
-	then
-	    echo "Creating: $config_link configuration link..."
-	    stow $config_link -t "$HOME/.config/$config_link"
-	else
-	    echo "Creating: $config_link not found!"
-	fi
-    done
+#===> Complete install function
+Complete_install(){
+    echo "[Complete_install]===> $1"
 }
 
-# Execute functions
 
-main(){
-    echo "--->LINK<---"
-    CREATE_CONFIG_DIR
-    CREATE_CONFIG_LINK
+#===> install aur
+Install_AUR(){
+    Start_install $AUR
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/$AUR.git
+    cd $AUR
+    makepkg -si     
+    Complete_install $AUR
 }
 
-main
+
+
+
