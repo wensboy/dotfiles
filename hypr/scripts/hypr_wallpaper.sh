bfd477b5489e78e9b8c5ba0ab9f114b45c971b24
wendisx <wendisx158@163.com>
2024-12-15 21:25:39 +0800

    update {hyprland}

--- hypr/scripts/hypr_wallpaper.sh
+++ hypr/scripts/hypr_wallpaper.sh
@@ -5,24 +5,24 @@ morning_wp=~/Pictures/wallpapers/yuanshan01.png
 afternoon_wp=~/Pictures/wallpapers/bluebeach.png
 dawn_wp=~/Pictures/wallpapers/cloud.png
 
-swww_opts="--transition-step 5 --transition-fps 60 --transition-type random"
+opts="--transition-type random --transition-fps 90 --transition-step 5"
 
 # kill exist swww-daemon
 swww kill
-
+swww-daemon
 
 # get current time
 cur=$(date +%H)
 
 case $cur in
 	00 | 01 | 02 | 03 | 04 | 05 | 06 | 07) 
-    	        swww-daemon && swww img $morning_wp $swww_opts
+    	        swww img $morning_wp $opts
 		;;
 	08 | 09 | 10 | 11 | 12 | 13 | 14 | 15) 
-		swww-daemon && swww img $afternoon_wp $swww_opts
+		swww img $afternoon_wp $opts
 		;;
 	16 | 17 | 18 | 19 | 20 | 21 | 22 | 23) 
-		swww-daemon && swww img $dawn_wp $swww_opts
+		swww img $dawn_wp $opts
 		;;
 esac
 
